cd6b79ca8cd5625cbcdb6e4f086d89ac0bd63439
JSewell0 <107957715+JSewell0@users.noreply.github.com>
2023-06-14 14:35:56 -0500

    Update Xsectiongraph3.cc

--- Xsectiongraph3.cc
+++ Xsectiongraph3.cc
@@ -6,6 +6,7 @@
 #include <TGraph.h>
 #include <TSpline.h>
 #include <TCanvas.h>
+
 using namespace std;
 
 // Function to split a string into tokens based on a delimiter
@@ -20,7 +21,7 @@ vector<string> split(string& str, char delimiter) {
 }
 
 TSpline3* file_process(string file_name){
-     ifstream file1(file_name);  // Replace "data.csv" with the path to your CSV file
+    ifstream file1(file_name);  // Replace "data.csv" with the path to your CSV file
 
     vector<double> col1_data;  // Vector to store the first column data
     vector<double> col2_data;  // Vector to store the second column data
@@ -52,12 +53,6 @@ TSpline3* file_process(string file_name){
     }
 
     TSpline3* spline1 = new TSpline3("spline1", dynamicArray1, dynamicArray2, numEntries1);
-
-    // if(file_name=="N2C1.csv"){
-    //   for(int i=0;i<numEntries1;i++){
-    //     cout<<dynamicArray1[i]<<" "<<dynamicArray2[i]<<endl;
-    //   }
-    // }
     
     return spline1;
    
@@ -66,18 +61,21 @@ TSpline3* file_process(string file_name){
 void graph_draw(TGraph* graph1, TString graph_title, TString x_title, TString y_title, TString out_file){
   
   TCanvas* canvas = new TCanvas("Xsection vs mu", "Xsection vs mu", 800, 800);
+  //gPad->SetLogy();
   
-  graph1->Draw("AP");
+  graph1->Draw("AL");
   graph1->SetTitle(graph_title);
   graph1->GetXaxis()->SetTitle(x_title);
   graph1->GetYaxis()->SetTitle(y_title);
+
+  graph1->SetLineWidth(3);
+  graph1->SetLineStyle(1);
+  graph1->SetLineColor(kBlue-4);
   
   canvas->SaveAs(out_file);
   delete canvas;
   
 }
-
-
 
 
 void Xsectiongraph3() {
@@ -131,104 +129,61 @@ void Xsectiongraph3() {
 	double yN3N1Zpos = N3N1Zpos->Eval(j);
 	double yN3N1Zneg = N3N1Zneg->Eval(j);
 
-	cout<<j<<endl;
-	cout<<"yN2N3: "<<yN2N3<<endl;
-	cout<<"yN2C1: "<<yN2C1<<endl;
-	cout<<"yN3C1: "<<yN3C1<<endl;
-	cout<<"yC1C1: "<<yC1C1<<endl;
-	cout<<"yN2N1hpos: "<<yN2N1hpos<<endl;
-	cout<<"yN2N1hneg: "<<yN2N1hneg<<endl;
-	cout<<"yN2N1Zpos: "<<yN2N1Zpos<<endl;
-	cout<<"yN2N1Zneg: "<<yN2N1Zneg<<endl;
-	cout<<"yN3N1hpos: "<<yN3N1hpos<<endl;
-	cout<<"yN3N1hneg: "<<yN3N1hneg<<endl;
-	cout<<"yN3N1Zpos: "<<yN3N1Zpos<<endl;
-	cout<<"yN3N1Zneg: "<<yN3N1Zneg<<endl<<endl;
+	// cout<<j<<endl;
+	// cout<<"yN2N3: "<<yN2N3<<endl;
+	// cout<<"yN2C1: "<<yN2C1<<endl;
+	// cout<<"yN3C1: "<<yN3C1<<endl;
+	// cout<<"yC1C1: "<<yC1C1<<endl;
+	// cout<<"yN2N1hpos: "<<yN2N1hpos<<endl;
+	// cout<<"yN2N1hneg: "<<yN2N1hneg<<endl;
+	// cout<<"yN2N1Zpos: "<<yN2N1Zpos<<endl;
+	// cout<<"yN2N1Zneg: "<<yN2N1Zneg<<endl;
+	// cout<<"yN3N1hpos: "<<yN3N1hpos<<endl;
+	// cout<<"yN3N1hneg: "<<yN3N1hneg<<endl;
+	// cout<<"yN3N1Zpos: "<<yN3N1Zpos<<endl;
+	// cout<<"yN3N1Zneg: "<<yN3N1Zneg<<endl<<endl;
 	
-	// p1[i] = yN2C1 * yN2N1Zpos + yN3C1 * yN3N1Zpos;
-	// p2[i] = yN2C1 * yN2N1hpos + yN3C1 * yN3N1hpos;
-	// p3[i] = yC1C1;
-	// p4[i] = yN2N3 * yN2N1Zpos * yN3N1Zpos;
-	// p5[i] = yN2N3 * yN2N1Zpos * yN3N1hpos + yN2N3 * yN2N1hpos * yN3N1Zpos;
-	// p6[i] = yN2N3 * yN2N1hpos * yN3N1hpos;
+	p1[i] = yN2C1 * yN2N1Zpos + yN3C1 * yN3N1Zpos;
+	p2[i] = yN2C1 * yN2N1hpos + yN3C1 * yN3N1hpos;
+	p3[i] = yC1C1;
+	p4[i] = yN2N3 * yN2N1Zpos * yN3N1Zpos;
+	p5[i] = yN2N3 * yN2N1Zpos * yN3N1hpos + yN2N3 * yN2N1hpos * yN3N1Zpos;
+	p6[i] = yN2N3 * yN2N1hpos * yN3N1hpos;
 
-	// n1[i] = yN2C1 * yN2N1Zneg + yN3C1 * yN3N1Zneg;
-	// n2[i] = yN2C1 * yN2N1hneg + yN3C1 * yN3N1hneg;
-	// n3[i] = yC1C1;
-	// n4[i] = yN2N3 * yN2N1Zneg * yN3N1Zneg;
-	// n5[i] = yN2N3 * yN2N1Zneg * yN3N1hneg + yN2N3 * yN2N1hneg * yN3N1Zneg;
-	// n6[i] = yN2N3 * yN2N1hneg * yN3N1hneg;
+	n1[i] = yN2C1 * yN2N1Zneg + yN3C1 * yN3N1Zneg;
+	n2[i] = yN2C1 * yN2N1hneg + yN3C1 * yN3N1hneg;
+	n3[i] = yC1C1;
+	n4[i] = yN2N3 * yN2N1Zneg * yN3N1Zneg;
+	n5[i] = yN2N3 * yN2N1Zneg * yN3N1hneg + yN2N3 * yN2N1hneg * yN3N1Zneg;
+	n6[i] = yN2N3 * yN2N1hneg * yN3N1hneg;
 
       }
 
-     // for(int i=0;i<size1;i++){
-     //   cout<<p1[i]<<endl;
-     // }
-     
-    // TGraph* WZp = new TGraph(size1, x, p1);
-    // TGraph* Whp = new TGraph(size1, x, p2);
-    // TGraph* WWp = new TGraph(size1, x, p3);
-    // TGraph* ZZp = new TGraph(size1, x, p4);
-    // TGraph* hZp = new TGraph(size1, x, p5);
-    // TGraph* hhp = new TGraph(size1, x, p6);
-    // TGraph* WZn = new TGraph(size1, x, n1);
-    // TGraph* Whn = new TGraph(size1, x, n2);
-    // TGraph* WWn = new TGraph(size1, x, n3);
-    // TGraph* ZZn = new TGraph(size1, x, n4);
-    // TGraph* hZn = new TGraph(size1, x, n5);
-    // TGraph* hhn = new TGraph(size1, x, n6);
+    TGraph* WZp = new TGraph(size1, x, p1);
+    TGraph* Whp = new TGraph(size1, x, p2);
+    TGraph* WWp = new TGraph(size1, x, p3);
+    TGraph* ZZp = new TGraph(size1, x, p4);
+    TGraph* hZp = new TGraph(size1, x, p5);
+    TGraph* hhp = new TGraph(size1, x, p6);
+    TGraph* WZn = new TGraph(size1, x, n1);
+    TGraph* Whn = new TGraph(size1, x, n2);
+    TGraph* WWn = new TGraph(size1, x, n3);
+    TGraph* ZZn = new TGraph(size1, x, n4);
+    TGraph* hZn = new TGraph(size1, x, n5);
+    TGraph* hhn = new TGraph(size1, x, n6);
 
     //graph_draw(TGraph* graph1,TString graph_title,TString x_title,TString y_title,TString out_file)    
-    //graph_draw(WZp,"WZ+MET+mu","mu GEV","XSection","WZ+mu.png");
-     
-    // TCanvas* canvas = new TCanvas("Xsection vs mu", "Xsection vs mu", 800, 800);
-
-    // WZp->Draw("AP");
-    // WZp->SetTitle("WZ+MET,+mu;mu GeV; XSection");
-    // canvas->SaveAs("WZ+mu.png");
-
-//   Whp->Draw("AP");
-    // Whp->SetTitle("Wh+MET,+mu;mu GeV; XSection");
-    //canvas->SaveAs("Wh+mu.png");
-
-    // WWp->Draw("AP");
-    //    WWp->SetTitle("WW+MET,+mu;mu GeV; XSection");
-    // canvas->SaveAs("WW+mu.png");
-
-    //ZZp->Draw("AP");
-    //ZZp->SetTitle("ZZ+MET,+mu;mu GeV; XSection");
-    //canvas->SaveAs("ZZ+mu.png");
-
-    //hZp->Draw("AP");
-    //hZp->SetTitle("hZ+MET,+mu;mu GeV; XSection");
-    //canvas->SaveAs("hZ+mu.png");
-
-    //hhp->Draw("AP");
-    //hhp->SetTitle("hh+MET,+mu;mu GeV; XSection");
-    //canvas->SaveAs("hh+mu.png");
-
-    //WZn->Draw("AP");
-    //WZn->SetTitle("WZ-MET,+mu;mu GeV; XSection");
-    //canvas->SaveAs("WZ-mu.png");
-
-    //Whn->Draw("AP");
-    //Whn->SetTitle("Wh-MET,+mu;mu GeV; XSection");
-    //canvas->SaveAs("Wh-mu.png");
-
-    //    WWn->Draw("AP");
-    //WWn->SetTitle("WW-MET,+mu;mu GeV; XSection");
-    //canvas->SaveAs("WW-mu.png");
-
-    //ZZn->Draw("AP");
-    //ZZn->SetTitle("ZZ-MET,+mu;mu GeV; XSection");
-    //canvas->SaveAs("ZZ-mu.png");
-
-    //hZn->Draw("AP");
-    //hZn->SetTitle("hZ-MET,+mu;mu GeV; XSection");
-    //canvas->SaveAs("hZ-mu.png");
-
-    // hhn->Draw("AP");
-    // hhn->SetTitle("hh-MET,+mu;mu GeV; XSection");
-    //canvas->SaveAs("hh-mu.png");
+    graph_draw(WZp,"WZ+MET+mu","mu GEV","XSection","WZ+mu.png");
+    graph_draw(Whp,"Wh+MET+mu","mu GEV","XSection","Wh+mu.png");
+    graph_draw(WWp,"WW+MET+mu","mu GEV","XSection","WW+mu.png");
+    graph_draw(ZZp,"ZZ+MET+mu","mu GEV","XSection","ZZ+mu.png");
+    graph_draw(hZp,"hZ+MET+mu","mu GEV","XSection","hZ+mu.png");
+    graph_draw(hhp,"hh+MET+mu","mu GEV","XSection","hh+mu.png");
+    graph_draw(WZn,"WZ-MET+mu","mu GEV","XSection","WZ-mu.png");
+    graph_draw(Whn,"Wh-MET+mu","mu GEV","XSection","Wh-mu.png");
+    graph_draw(WWn,"WW-MET+mu","mu GEV","XSection","WW-mu.png");
+    graph_draw(ZZn,"ZZ-MET+mu","mu GEV","XSection","ZZ-mu.png");
+    graph_draw(hZn,"hZ-MET+mu","mu GEV","XSection","hZ-mu.png");
+    graph_draw(hhn,"hh-MET+mu","mu GEV","XSection","hh-mu.png");
     
  }
